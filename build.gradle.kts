2615d68026787688cb1966153e0c44832459f63c
Sam Eckert <github@sam0711er.com>
2025-02-23 23:55:29 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     implementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
     implementation("io.github.cdimascio:dotenv-java:2.2.0")
     implementation("org.yaml:snakeyaml:2.2")
-    implementation("com.github.bastiaanjansen:otp-java:latest.release")
+    implementation("com.github.bastiaanjansen:otp-java:2.1.0")
 }
 
 tasks.test {
